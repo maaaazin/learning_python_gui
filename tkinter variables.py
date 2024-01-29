e8207ce8872df55c7acd43e1aa53aadfff6f24f8
Maazin Kazi <maazinazim@gmail.com>
2024-01-30 04:05:15 +0530

    incomplete...will complete later

---
+++ tkinter variables.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Window
+window = tk.Tk()
+
+# Tkinter has basic inbuilt variables that are used to store strings, int and bools
+
+
+
+
+# Running
+window.mainloop()
